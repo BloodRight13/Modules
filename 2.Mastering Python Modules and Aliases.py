26a5004a295c42b5fa0c1e6e223bdce1724b3c10
BloodRight13 <jwpeterson0648@gmail.com>
2024-08-29 19:14:39 -0500

    First Commit

---
+++ 2.Mastering Python Modules and Aliases.py
@@ -0,0 +1,12 @@
+# Task 1
+import text_utils as tu
+
+s = input("Enter a string: ")
+
+reversed_string = tu.reverse_string(s)
+
+print(reversed_string)
+
+capitalized_string = tu.capitalize_string(s)
+
+print(capitalized_string)
---
+++ mood_responses.py
@@ -0,0 +1,8 @@
+
+def mood_response(mood):
+    if mood == "good":
+        print("That is great to hear.")
+    elif mood == "sad":
+        print("I am sorry to here that.")
+    else:
+        print("Sorry I didn't catch what you said.")
